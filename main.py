8f3a0842419894cde3e4563d63e6c232d140cf11
aahanad <150878761+aahanad@users.noreply.github.com>
2024-02-27 18:29:41 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+
 import pgzrun
 WIDTH=600
 HEIGHT=600
+score=0
 ship=Actor("ship")
 ship.x=300
 ship.y=400
@@ -12,15 +14,21 @@ for i in range(7):
     bug.y=200
     bugs.append(bug)
     
-
+ship.dead=False
 def draw():
     screen.fill("slate gray")
-    ship.draw()
+    if ship.dead==False:
+        ship.draw()
+    else:
+        screen.draw.text("game over!",(300,300),color="red")
+
     for b in bullets:
         b.draw()
     for bug in bugs:
         bug.draw()
+    screen.draw.text (str(score),(15,15),color=  "deep sky blue")
 def update():
+    global score
     for b in bullets:
         b.y=b.y-5
     if keyboard.w:
@@ -42,5 +50,8 @@ def update():
             if bullet.colliderect(b):
                 bullets.remove(bullet)
                 bugs.remove(b)
-
-pgzrun.go()
+                score=score+1
+    
+        if ship.colliderect(b):
+            ship.dead=True
+           
